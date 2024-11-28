13f39280bed40875b2f4892a466282528d4d7d27
“[rezatahseenfiverr]” <[rezatahseenfiverr2gmail.com]>
2024-11-29 00:34:09 +0600

    M

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Set the base URL for the app (use relative paths)
+  build: {
+    outDir: 'dist', // Optional, but this ensures the build folder is named correctly
+  },
+  server: {
+    // Handle SPA routing and fallback for routes to index.html
+    historyApiFallback: true,
+  },
 })
