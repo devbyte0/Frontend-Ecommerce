35d01a5b1f289958112c0c903b1e6103f0085342
“[rezatahseenfiverr]” <[rezatahseenfiverr2gmail.com]>
2024-11-30 03:14:37 +0600

    M

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import { createRequire } from "module";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const require = createRequire(import.meta.url);
 const app = express();
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve static files from the dist folder
 app.use(express.static(path.join(__dirname, "dist")));
