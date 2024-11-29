5433986781b88a17a99eb6e732ab7fd17ad7687c
“[rezatahseenfiverr]” <[rezatahseenfiverr2gmail.com]>
2024-11-30 02:07:37 +0600

    M

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,37 +1,41 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
-import ContactUs from "../pages/ContactUs";
-import Login from "../pages/Login";
-import SignUp from "../pages/SignUp";
-import Layout from "../layout/Layout";
+import {
+    createBrowserRouter
+  } from "react-router-dom";
+  import Home from "../pages/Home";
+  import ContactUs from "../pages/ContactUs";
+  import Login from "../pages/Login";
+  import SignUp from "../pages/SignUp";
+  import Layout from "../layout/Layout";
 import Products from "../pages/Products";
-import ProductView from "../pages/ProductView";
-import CartPage from "../pages/Cart";
-import CheckoutPage from "../pages/Checkout";
-import UserLayout from "../layout/UserLayout";
-import UserProfile from "../pages/User";
-
 import AdminLayout from "../layout/AdminLayout";
 import AdminLogin from "../pages/AdminLogin";
 import AdminDashBoard from "../pages/AdminDashBoard";
 import Dashboard from "../pages/Dashboard";
 import AdminCrudPage from "../pages/AdminCrudPage";
+
 import AdminProfile from "../pages/AdminProfile";
 import UserCrudPage from "../pages/UserCrudPage";
 import ProductAdminPage from "../pages/ProductAdminPage";
 import ChatList from "../pages/ChatList";
 import MessagePage from "../pages/MessagePage";
 import ProductCRUDPage from "../pages/ProductCrudPage";
-import ProductEdit from "../pages/ProductEdit";
-import ProductCreate from "../pages/ProductCreate";
+import ProductView from "../pages/ProductView";
 import ColorManagement from "../pages/Colors";
 import SizeManagement from "../pages/Sizes";
 import CategoryManagement from "../pages/Categories";
 import GenderManagement from "../pages/Gender";
+import ProductCreate from "../pages/ProductCreate";
 import BadgeManagement from "../pages/Badges";
+import ProductEdit from "../pages/ProductEdit";
 import CouponManagement from "../pages/Coupon";
+import CartPage from "../pages/Cart";
 import SliderManagement from "../pages/AdminSlides";
 import RelatedProductManagement from "../pages/RelatedProduct";
+import CheckoutPage from "../pages/Checkout";
+import UserProfile from "../pages/User";
+import UserLayout from "../pages/UserLayout";
+
+
 
 export const router = createBrowserRouter([
   {
